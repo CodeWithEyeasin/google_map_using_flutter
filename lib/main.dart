c7aed914aaaf74140be2d754f9271ee940815819
CodeWithEyeasin <mreyeasinarafat@gmail.com>
2024-05-03 22:43:15 +0600

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 main(){
   runApp(const MovieApp());
@@ -26,16 +25,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  Position? position;
+
+  void initState(){
+    super.initState();
+    _onScreenStart();
+    _listenCurrentLocation();
+
+  }
+
+  //GPS Service Enable/disable
+  //App permission enable?
+  //getlocation -once
+  // listen location
+
+  Future<void> _onScreenStart() async{
+    bool isEnable =await Geolocator.isLocationServiceEnabled();
+print(isEnable);
+
+print(await Geolocator.getLastKnownPosition());
+    LocationPermission permission= await Geolocator.checkPermission();
+
+if(permission==LocationPermission.whileInUse || permission==LocationPermission.always){
+   position=await Geolocator.getCurrentPosition();
+}else{
+  LocationPermission requestStatus= await Geolocator.requestPermission();
+  if(requestStatus==LocationPermission.whileInUse || requestStatus==LocationPermission.always){
+    _onScreenStart();
+
+  }
+}
+
+
+
+  }
+  void _listenCurrentLocation(){
+    Geolocator.getPositionStream(locationSettings: LocationSettings(
+      accuracy: LocationAccuracy.best,
+      distanceFilter: 1,
+      // timeLimit: Duration(seconds: 3),
+      /// (this is only for real device)
+    )).listen((p) {
+print(p);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: GoogleMap(
-        zoomControlsEnabled: true,
+      body: Center(
+        child: Text(
+            'Current Location ${position?.latitude }, ${position?.longitude}'),
 
-        initialCameraPosition: CameraPosition(target: LatLng(23.938223, 90.391265),),
       ),
     );
   }
